70de4c1b0259ea6af02221bd9e09c92b1d229336
yash-a-18 <yashamethiya2001@gmail.com>
2021-02-06 18:32:29 +0530

    Add Customer Logout and session

--- VehicleRentingSystem/CustomerHome/views.py
+++ VehicleRentingSystem/CustomerHome/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from CustomerHome.models import Customer
 
+isLogin = False
+
 # Create your views here.
 def index(request):
     index=[1,2,3,4,5,6];
+    global isLogin
+    if('user_email' not in request.session and isLogin):
+        isLogin = False
+        Message = "Successfully Logged Out!!"
+        return render(request,'index.html',{'Message':Message,'p':index})
     return render(request,'index.html',{'p':index})
 
 def signin(request):
@@ -14,7 +21,23 @@ def signin(request):
 def register(request):
     return render(request,'register.html')
 
+def LoginAuthentication(request):
+    global isLogin
+    login_email=request.POST.get('login_email','')
+    login_password=request.POST.get('login_password','')
+    # customer = Customer.objects.all()
+
+    result = Customer.objects.filter(customer_email=login_email,customer_password=login_password)
+    if result.exists():
+        request.session['user_email'] = login_email
+        isLogin = True
+        return redirect('/Home/')
+    else:
+        Message = "Invalid Email or password!!"
+        return render(request,'SignIn.html',{'p':index,'Message':Message})
+
 def RegisterCustomer(request):
+    global isLogin
     customer_firstname=request.POST.get('customer_firstname','')
     customer_lastname=request.POST.get('customer_lastname','')
     customer_dob=request.POST.get('customer_dob','')
@@ -36,13 +59,24 @@ def RegisterCustomer(request):
     customer_pincode=customer_pincode,customer_license=customer_license)
     
     customer.save()
-    Message = "Successfully Registered"
-    index=[1,2,3,4,5,6];
-    return render(request,'Home.html',{'p':index,'Message':Message,'customer':customer,'customer_license':customer.customer_license})
+    request.session['user_email'] = customer_email
+    isLogin = True
+    return redirect('/Home/')
+
+def Logout(request):
+    del request.session['user_email']
+    Message = "Successfully Logged Out!!"
+    index=[1,2,3,4,5,6]
+    return redirect('/')
 
 def Home(request):
-    index=[1,2,3,4,5,6];
-    return render(request,'Home.html',{'p':index})
+    if('user_email' not in request.session):
+        return redirect('/')
+    customer_email = request.session.get('user_email')
+    customer = Customer.objects.get(customer_email=customer_email)
+    index=[1,2,3,4,5,6]
+    Message="Welcome Aboard!!"
+    return render(request,'Home.html',{'p':index,'Message':Message,'customer':customer})
 
 def about_us(request):
     return HttpResponse('About Us')
